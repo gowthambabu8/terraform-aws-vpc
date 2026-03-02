cba1f4df648b02bf15b2ed97bdcc27cdd4289cf8
Gowtham <gowthambabu8@gmail.com>
2026-03-02 23:20:41 +0530

    terraform vpc changes

--- peering.tf
+++ peering.tf
@@ -15,9 +15,17 @@ resource "aws_vpc_peering_connection" "main" {
 }
 
 # vpc peering route
-resource "aws_route" "vpc-peering" {
+resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1: 0
   route_table_id = aws_route_table.public.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+}
+
+# vpc peering route
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1: 0
+  route_table_id = data.aws_route_table.default.id
+  destination_cidr_block = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
 }
