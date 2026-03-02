864fae2f20d65baef54008bdc0b89648a6245079
Gowtham <gowthambabu8@gmail.com>
2026-03-02 19:29:45 +0530

    terraform vpc changes

--- main.tf
+++ main.tf
@@ -99,6 +99,7 @@ resource "aws_route" "public" {
   route_table_id = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.main.id
+  depends_on = [ aws_internet_gateway.main.id ]
 }
 
 # EIP creation
