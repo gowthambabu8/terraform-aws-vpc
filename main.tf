c2e6d7642d58529c5d800e64ffbe71c1105bcbc3
Gowtham <gowthambabu8@gmail.com>
2026-03-02 19:25:16 +0530

    terraform vpc changes

--- main.tf
+++ main.tf
@@ -5,12 +5,13 @@ resource "aws_vpc" "main" {
   tags = local.vpc_final_tags
 }
 
-resource "aws_internet_gateway" "name" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = local.igw_final_tags
 }
 
-resource "aws_subnet" "name" {
+# public subnet
+resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidr)
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr[count.index]
@@ -22,4 +23,120 @@ resource "aws_subnet" "name" {
     {
         Name = "${var.project}-${var.environment}-public-${local.azs_names[count.index]}"
     } )
+}
+
+# private subnet
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = local.azs_names[count.index]
+  map_public_ip_on_launch = false
+  # Name = roboshop-dev-public-us-east-1a
+  tags = merge(
+    local.subnet_final_tags,
+    {
+        Name = "${var.project}-${var.environment}-private-${local.azs_names[count.index]}"
+    } )
+}
+
+# database subnet
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = local.azs_names[count.index]
+  map_public_ip_on_launch = false
+  
+  tags = merge(
+    local.subnet_final_tags,
+    {
+        Name = "${var.project}-${var.environment}-database-${local.azs_names[count.index]}"
+    } )
+}
+
+# public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public"
+    },
+    var.public_route_table_tags
+  )
+}
+
+# private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private"
+    },
+    var.private_route_table_tags
+  )
+}
+
+# database route table
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database"
+    },
+    var.database_route_table_tags
+  )
+}
+
+# public route
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+# EIP creation
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat"
+    },
+    var.nat_route_table_tags
+  )
+}
+
+# NAT Gateway 
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public[0].id # we are creating nat gateway in us-east1a
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat"
+    },
+    var.nat_route_table_tags
+  )
+}
+
+# private route
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private.id
+  nat_gateway_id = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  
+}
+
+# database route
+resource "aws_route" "database" {
+  route_table_id = aws_route_table.database.id
+  nat_gateway_id = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
 }
--- variables.tf
+++ variables.tf
@@ -26,7 +26,36 @@ variable "public_subnet_cidr" {
   default = [ "10.0.1.0/24","10.0.2.0/24" ]
 }
 
+variable "private_subnet_cidr" {
+  type = list(string)
+  default = [ "10.0.11.0/24","10.0.12.0/24" ]
+}
+
+variable "database_subnet_cidr" {
+  type = list(string)
+  default = [ "10.0.21.0/24","10.0.22.0/24" ]
+}
 variable "subnet_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  type = map(string)
+  default = {}
+}
+
+variable "nat_route_table_tags" {
   type = map(string)
   default = {}
 }
