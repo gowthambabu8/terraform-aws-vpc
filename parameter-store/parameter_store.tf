70103e9e9e08a613a141e0c597561e4344242dd1
Gowtham <gowthambabu8@gmail.com>
2026-03-23 23:37:06 +0530

    terraform module changes vpc

--- parameter-store/parameter_store.tf
+++ parameter-store/parameter_store.tf
@@ -1,23 +1,23 @@
 resource "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project}/${var.environment}/vpc_id"
   type="String"
-  value = aws_vpc.main.id
+  value = var.vpc_id
 }
 
 resource "aws_ssm_parameter" "public_subnet" {
   name = "/${var.project}/${var.environment}/public_subnet"
   type = "StringList"
-  value = join(",",aws_subnet.public[*].id)
+  value = join(",",var.public_subnet_cidr)
 }
 
 resource "aws_ssm_parameter" "private_subnet" {
   name = "/${var.project}/${var.environment}/private_subnet"
   type = "StringList"
-  value = join(",",aws_subnet.private[*].id)
+  value = join(",",var.private_subnet_cidr)
 }
 
 resource "aws_ssm_parameter" "database_subnet" {
   name = "/${var.project}/${var.environment}/database_subnet"
   type = "StringList"
-  value = join(",",aws_subnet.database[*].id)
+  value = join(",",var.database_subnet_cidr)
 }
--- parameter-store/variables.tf
+++ parameter-store/variables.tf
@@ -6,61 +6,18 @@ variable "environment" {
   type = string
 }
 
-variable "cidr_block" {
+variable "vpc_id" {
   type = string
-  default = "10.0.0.0/16"
-}
-
-variable "vpc_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "igw_tags" {
-  type = map(string)
-  default = {}
 }
 
 variable "public_subnet_cidr" {
   type = list(string)
-  default = [ "10.0.1.0/24","10.0.2.0/24" ]
 }
 
 variable "private_subnet_cidr" {
   type = list(string)
-  default = [ "10.0.11.0/24","10.0.12.0/24" ]
 }
 
 variable "database_subnet_cidr" {
   type = list(string)
-  default = [ "10.0.21.0/24","10.0.22.0/24" ]
-}
-variable "subnet_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "public_route_table_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "private_route_table_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "database_route_table_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "nat_route_table_tags" {
-  type = map(string)
-  default = {}
-}
-
-variable "is_peering_required" {
-  default = true
-  type = bool
 }
