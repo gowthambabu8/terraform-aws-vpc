fe9d93a36e3c91bfa13171d0ee2e61776cb61047
Gowtham <gowthambabu8@gmail.com>
2026-03-24 19:15:10 +0530

    terraform aws sg changes

--- vpc/main.tf
+++ vpc/main.tf
@@ -162,3 +162,27 @@ resource "aws_route_table_association" "database" {
   route_table_id = aws_route_table.database.id
   subnet_id = aws_subnet.database[count.index].id
 }
+
+resource "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+  type="String"
+  value = aws_vpc.main.id
+}
+
+resource "aws_ssm_parameter" "public_subnet" {
+  name = "/${var.project}/${var.environment}/public_subnet"
+  type = "StringList"
+  value = join(",",aws_subnet.public[*].id)
+}
+
+resource "aws_ssm_parameter" "private_subnet" {
+  name = "/${var.project}/${var.environment}/private_subnet"
+  type = "StringList"
+  value = join(",",aws_subnet.private[*].cidr_block)
+}
+
+resource "aws_ssm_parameter" "database_subnet" {
+  name = "/${var.project}/${var.environment}/database_subnet"
+  type = "StringList"
+  value = join(",",aws_subnet.database[*].cidr_block)
+}
